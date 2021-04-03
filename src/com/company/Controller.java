bba5955fcf86f7fe6b560599319c9bbd02e994e4
Alec Franz G. Cresencio <alec_cresencio@dlsu.edu.ph>
2021-04-03 21:46:34 +0800

    switched pw field instead of text field

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -16,7 +17,8 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Controller {
-    public TextField usernameField, pwField;
+    public TextField usernameField;
+    public PasswordField pwField;
     public Label incorrectLabel;
     public UserObject currentUser;
     public Button loginButton;
